d55c6d6b9dd6f29900b2b6f9b55a851b113dba42
unknown <you@example.com>
2021-09-06 01:10:50 +0100

    ...

--- app.js
+++ app.js
@@ -25,39 +25,28 @@ playBtn.addEventListener('click', () => {
     disk.classList.toggle('play');
 })
 
-setInterval(() => {
-    seekBar.value = music.currentTime;
-    currentTime.innerHTML = formatTime(music.currentTime);
-}, 500)
 
-seekBar.addEventListener('change', () => {
-    music.currentTime = seekBar.value;
-})
-
-forwardBtn.addEventListener('click', () => {
-    if (currentMusic >= songs.length - 1) {
-        currentMusic = 0;
-    } else {
-        currentMusic++;
-    }
-    setMusic(currentMusic);
-    playMusic();
-})
-
-backwardBtn.addEventListener('click', () => {
-    if (currentMusic <= 0) {
-        currentMusic = songs.length - 1;
-    } else {
-        currentMusic--;
-    }
-    setMusic(currentMusic);
-    playMusic();
-})
 
 const playMusic = () => {
     music.play();
     playBtn.classList.remove('pause');
     disk.classList.add('play');
+}
+const setMusic = (i) => {
+    seekBar.value = 0; // set range slide value to 0;
+    let song = songs[i];
+    currentMusic = i;
+    music.src = song.path;
+
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
+
+    currentTime.innerHTML = '00:00';
+    setTimeout(() => {
+        seekBar.max = music.duration;
+        musicDuration.innerHTML = formatTime(music.duration);
+    }, 300);
 }
 
 setInterval(() => {
@@ -67,3 +56,17 @@ setInterval(() => {
         forwardBtn.click();
     }
 }, 500)
+
+
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if (min < 10) {
+        min = `0${min}`;
+    }
+    let sec = Math.floor(time % 60);
+    if (sec < 10) {
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;
+}
